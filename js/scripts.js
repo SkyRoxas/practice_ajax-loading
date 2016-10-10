de7f8f969cc8577e05bc71c572df31d9c1d8c696
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-10 19:00:24 +0800

    ajax html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+jQuery(document).ready(function($) {
+    $.ajax({
+        url: "/templates/nav-reply/nav-reply.html",
+        success: function(result) {
+            $(".chat-nav-ajax").html(result);
+        }
+    });
+})
