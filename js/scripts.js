655e54e7bcc2e5299e240cf66bad90a5ab1a0a2c
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-11 15:33:07 +0800

    scroll to bottom

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ var replyObj = new Object();
 var messageArray = [];
 
 replyObj.message = messageArray;
+
+
 //時間
 var mydate = new Date();
 var time =
@@ -20,6 +22,7 @@ $.ajax({
         $('.chat-nav-ajax').html(result);
     }
 });
+
 function ajaxGet($project) {
     $.ajax({
         url: '/templates/' + $project + '.html',
@@ -28,6 +31,13 @@ function ajaxGet($project) {
         }
     });
 }
+
+//scroll置最底
+function scollToBottom($element) {
+    $($element).scrollTop($($element).scrollTop() + $($element).height());
+}
+
+
 
 
 $(document).ajaxComplete(function() {
@@ -41,6 +51,22 @@ $(document).ajaxComplete(function() {
     })
 })
 
+
+
+//好友訊息
+function friendMessage() {
+    setTimeout(function() {
+        $('.chat-content .chat-wrap').append('<div class ="message friend-message left"><div class ="field-content"><div class ="field-item field-message"><p>你好安安</p></div><div class ="field-item field-time"><h5>' + time + '</h5></div></div>');
+        scollToBottom('.chat-content');
+    }, 2000);
+
+
+}
+
+
+
+
+//訊息按鈕 把資訊導入物件內
 function replybtn() {
     var messageObj = new Object;
     messageObj.user = "you";
@@ -51,23 +77,28 @@ function replybtn() {
     console.log(JSON.stringify(replyObj));
     $.each(replyObj, function($key, $value) {
         if ($key == "message") {
-          //wrap
-          var group = $value.length;
-          $('.chat-content .chat-wrap').append('<div class ="message message-'+group+' '+"right"+'"><div class ="field-content"></div></div>');
-          //取值加入wrap
+            //wrap
+            var group = $value.length;
+            $('.chat-content .chat-wrap').append('<div class ="message message-' + group + ' ' + "right" + '"><div class ="field-content"></div></div>');
+            //取值加入wrap
             $.each($value[$value.length - 1], function($key, $value) {
                 if ($key == "time") {
-                      $('.chat-content').find('.message-'+group).children('.field-content').append('<div class ="field-item field-time"><h5>'+$value+'</h5></div>');
+                    $('.chat-content').find('.message-' + group).children('.field-content').append('<div class ="field-item field-time"><h5>' + $value + '</h5></div>');
                 }
                 if ($key == "message") {
-                    $('.chat-content').find('.message-'+group).children('.field-content').append('<div class ="field-item field-message"><p>'+$value+'</p></div>');
+                    $('.chat-content').find('.message-' + group).children('.field-content').append('<div class ="field-item field-message"><p>' + $value + '</p></div>');
                 }
             })
         }
+        $('textarea').val(null);
     })
-
+    scollToBottom('.chat-content')
+    friendMessage();
 }
 
+
+
+//貼圖按鈕 把資訊導入物件內
 function imgbtn() {
     var messageObj = new Object;
     messageObj.user = "you";
@@ -78,18 +109,20 @@ function imgbtn() {
     console.log(JSON.stringify(replyObj));
     $.each(replyObj, function($key, $value) {
         if ($key == "message") {
-          //wrap
-          var group = $value.length;
-          $('.chat-content .chat-wrap').append('<div class ="message message-'+group+' '+"right"+'"><div class ="field-content"></div></div>');
-          //取值加入wrap
+            //wrap
+            var group = $value.length;
+            $('.chat-content .chat-wrap').append('<div class ="message message-' + group + ' ' + "right" + '"><div class ="field-content"></div></div>');
+            //取值加入wrap
             $.each($value[$value.length - 1], function($key, $value) {
                 if ($key == "time") {
-                      $('.chat-content').find('.message-'+group).children('.field-content').append('<div class ="field-item field-time"><h5>'+$value+'</h5></div>');
+                    $('.chat-content').find('.message-' + group).children('.field-content').append('<div class ="field-item field-time"><h5>' + $value + '</h5></div>');
                 }
                 if ($key == "message") {
-                    $('.chat-content').find('.message-'+group).children('.field-content').append('<div class ="field-item field-message message-image"><img src ="' + $value + '" style ="max-width:150px;"></div>');
+                    $('.chat-content').find('.message-' + group).children('.field-content').append('<div class ="field-item field-message message-image"><img src ="' + $value + '" style ="max-width:150px;"></div>');
                 }
             })
         }
     })
+    scollToBottom('.chat-content')
+    friendMessage();
 }
